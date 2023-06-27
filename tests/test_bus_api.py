e52550a7c11d924726a3fff25995363de1d1d39f
Daniel McKnight <daniel@neongecko.com>
2023-06-27 11:12:18 -0700

    Remove references in tests missed in #29

--- tests/test_bus_api.py
+++ tests/test_bus_api.py
@@ -30,7 +30,7 @@ import os
 import sys
 import unittest
 
-from ovos_bus_client import Message
+from mycroft_bus_client import Message
 from ovos_utils.messagebus import FakeBus
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
